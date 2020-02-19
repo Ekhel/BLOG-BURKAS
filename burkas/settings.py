6820e23c4261c27b4eb4626bed5421152ec508d0
ekhel <blackmanskill@gmail.com>
2020-02-19 20:23:37 +0900

    change

--- burkas/settings.py
+++ burkas/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 ADMIN_SITE_HEADER = "BURKAS BACKEND"
