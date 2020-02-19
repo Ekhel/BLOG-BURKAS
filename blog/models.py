c098cb2982723e4d32620524b5be31027190f6c4
ekhel <blackmanskill@gmail.com>
2020-02-19 20:27:47 +0900

    media_del

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,6 @@ class Article(models.Model):
     slug = models.SlugField()
     deskripsi = models.CharField(max_length=250)
     body = models.TextField()
-    gambar = models.ImageField(upload_to='image_article/', default='default.jpg')
     video = models.CharField(max_length=200)
     date_created = models.DateField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
