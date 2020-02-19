87c868053a394df2e99e1b100964d731f6016071
ekhel <blackmanskill@gmail.com>
2020-02-19 18:09:28 +0900

    bgd

--- burkas/settings.py
+++ burkas/settings.py
@@ -127,12 +127,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 ADMIN_SITE_HEADER = "BURKAS BACKEND"
 ADMIN_SITE_INDEX = "Administrator"
